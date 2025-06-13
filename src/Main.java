e70a6eb697adcff8080d39b0ff75a246a9c723c3
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-06-13 12:55:21 +0300

    задача 5.2.15 вторая проверка

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-
-        List<Integer> list = new ArrayList<>();
-
         String[] numbers = s.split(" ");
-        for (String number : numbers) {
-            list.add(Integer.valueOf(number));
-        }
 
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (i % 2 == 0) {
-                list.remove(i);
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0) {
+                deque.addFirst(Integer.parseInt(numbers[i]));
             }
         }
 
-        Collections.reverse(list);
-
-        System.out.println(list);
+        for (int i : deque) {
+            System.out.print(i + " ");
+        }
 
         sc.close();
     }
